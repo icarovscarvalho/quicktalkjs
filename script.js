8533144cdc7313dfd3a81f9430704da321a04493
Ícaro Carvalho <icarovscarvalho@gmail.com>
2024-07-28 20:52:05 -0300

    Adiciona valores do usuário a mensagem e email

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const modal = document.getElementById("modal-container");
 const alertDiv = document.getElementById("alert");
 const commentsArea = document.getElementById("comments-area");
-const boxContainer = document.getElementById("box-comments")
-const ifComments = document.getElementsByClassName("if-comments")[0]
+const boxContainer = document.getElementById("box-comments");
+const messageValue = document.getElementById("messageValue");
+const emailValue = document.getElementById("emailValue");
 
 function activeModal() {
     modal.style.display = "flex"
@@ -19,13 +20,21 @@ function sendComment() {
     createComment()
 }
 
+function userMessage() {
+    return messageValue.value
+}
+
+function userEmail() {
+    return emailValue.value
+}
+
 function createComment() {
     const divContainer = document.createElement("div")
     divContainer.classList.add("if-comments")
     boxContainer.appendChild(divContainer)
 
     const message = document.createElement('p')
-    message.innerText = 'Uma bela mesnagem vai vir aqui'
+    message.innerText = userMessage()
     divContainer.appendChild(message)
 
     const div = document.createElement("div")
@@ -37,7 +46,6 @@ function createComment() {
     div.appendChild(span)
 
     const email = document.createElement("p")
-    email.innerText = "seuemail@email.com"
+    email.innerText = userEmail()
     div.appendChild(email)
-
 }
