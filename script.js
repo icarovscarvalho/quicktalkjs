4812664e9b99215ffe9cef02b61be86c50fad6ca
Ícaro Carvalho <icarovscarvalho@gmail.com>
2024-07-28 16:09:25 -0300

    Adiciona áera de comentários e lógica de abertura do Modal

---
+++ script.js
@@ -0,0 +1,9 @@
+const modal = document.getElementById("modal-container")
+
+function activeModal() {
+    modal.style.display = "flex"
+}
+
+function closeModal() {
+    modal.style.display = "none"
+}
