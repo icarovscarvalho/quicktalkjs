ca1d2cb79104726035f050d644becb23d5f651c2
Ícaro Carvalho <icarovscarvalho@gmail.com>
2024-07-28 21:37:42 -0300

    Adiciona estilo quando sem comentários e Fixed - Faz a verificação de Comentários

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const main = document.getElementById("main")
 const modal = document.getElementById("modal-container");
 const alertDiv = document.getElementById("alert");
 const commentsArea = document.getElementById("comments-area");
@@ -29,23 +30,32 @@ function userEmail() {
 }
 
 function createComment() {
-    const divContainer = document.createElement("div")
-    divContainer.classList.add("if-comments")
-    boxContainer.appendChild(divContainer)
+    const messageReturn = userMessage()
+    const emailReturn = userEmail()
+    main.classList.remove('no-comments')
 
-    const message = document.createElement('p')
-    message.innerText = userMessage()
-    divContainer.appendChild(message)
+    if(messageReturn.length > 0 && emailReturn.length > 0) {
+        const divContainer = document.createElement("div")
+        divContainer.classList.add("if-comments")
+        boxContainer.appendChild(divContainer)
 
-    const div = document.createElement("div")
-    div.classList.add("like-email")
-    divContainer.appendChild(div)
+        const message = document.createElement('p')
+        message.innerText = messageReturn
+        divContainer.appendChild(message)
 
-    const span = document.createElement("span")
-    span.innerHTML = '<ion-icon name="heart-outline"></ion-icon>'
-    div.appendChild(span)
+        const div = document.createElement("div")
+        div.classList.add("like-email")
+        divContainer.appendChild(div)
 
-    const email = document.createElement("p")
-    email.innerText = userEmail()
-    div.appendChild(email)
+        const span = document.createElement("span")
+        span.innerHTML = '<ion-icon name="heart-outline"></ion-icon>'
+        div.appendChild(span)
+
+        const email = document.createElement("p")
+        email.innerText = emailReturn
+        div.appendChild(email)
+    } else {
+        alert('Você precisa Preencher os Campos de Mensagem e Email corretamente')
+        return
+    } 
 }
