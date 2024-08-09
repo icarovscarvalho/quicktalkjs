812694a064c92610ddbc397b9b50e60cf91db411
Icaro <icarovscarvalho@gmail.com>
2024-08-08 21:14:32 -0300

    Correção - Comentários agora são independentes e separados por ids únicos / lógica do like interage com o array

--- script.js
+++ script.js
@@ -8,16 +8,7 @@ const messageValue = document.getElementById("messageValue");
 const emailValue = document.getElementById("emailValue");
 
 
-let dataArr = []
 let usersDatas = []
-
-function saveUserDatas() {
-    return {
-        comment:userMessage(),
-        email:userEmail(),
-        isLike:false
-    }
-}
 
 function activeModal() {
     clearDatas()
@@ -50,30 +41,43 @@ function userEmail() {
     return emailValue.value
 }
 
-function isLike(e) {
-    const arrPos = usersDatas.length-1
-    const newComment = usersDatas[arrPos]
+const isLike = (e, id, i) => {
+    const newComment = usersDatas[i]
 
     if (newComment.isLike == false) {
         newComment.isLike = true
         e.innerHTML = '<ion-icon name="heart"></ion-icon>'
+        newComment.likes.push(id)
     } else {
         newComment.isLike = false
         e.innerHTML = '<ion-icon name="heart-outline"></ion-icon>'
+        newComment.likes.pop()
     }
+}
+
+function createID() {
+    const id = new Date().getTime()
+    return id
 }
 
 function createObjComment() {
     return {
+        id:createID(),
         message: userMessage(),
         mail: userEmail(),
+        likes:[],
         isLike:false
     }
 }
 
 function createElements() {
     console.log("criando Element")
+    
+
     for(i in usersDatas) {
+        const id = usersDatas[i].id
+        const likeLength = usersDatas[i].likes.length
+
         const divContainer = document.createElement("div")
         divContainer.classList.add("if-comments")
         boxComments.appendChild(divContainer)
@@ -87,18 +91,22 @@ function createElements() {
         divContainer.appendChild(div)
 
         const likeBtn = document.createElement('button')
-        likeBtn.innerHTML = '<ion-icon name="heart-outline"></ion-icon>'
-        likeBtn.setAttribute('onClick', 'isLike(this)')
+        likeBtn.innerHTML = likeLength>0? '<ion-icon name="heart"></ion-icon>' : '<ion-icon name="heart-outline"></ion-icon>'
+        likeBtn.onclick = () => {
+            isLike(likeBtn, id, i)
+        }
         div.appendChild(likeBtn)
 
         const likeNum = document.createElement('p')
-        likeNum.innerText = 0
+        likeNum.innerText = likeLength
         div.appendChild(likeNum)
 
         const email = document.createElement("p")
         email.innerText = usersDatas[i].mail
         div.appendChild(email)
     }
+
+    console.log(usersDatas)
 }
 
 function removeComments() {
